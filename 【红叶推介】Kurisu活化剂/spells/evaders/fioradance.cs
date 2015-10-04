7347d6cf018cc53506693d4e8925c6c893e7162a
ergou <654955321@qq.com>
2015-10-04 19:56:31 +0800

    new

---
+++ 【红叶推介】Kurisu活化剂/spells/evaders/fioradance.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace Activator.Spells.Evaders
+{
+    class fioradance : spell
+    {
+        internal override string Name
+        {
+            get { return "fioradance"; }
+        }
+
+        internal override string DisplayName
+        {
+            get { return "Blade Waltz | R"; }
+        }
+
+        internal override float Range
+        {
+            get { return 400f; }
+        }
+
+        internal override MenuType[] Category
+        {
+            get { return new[] { MenuType.SelfLowHP, MenuType.Zhonyas }; }
+        }
+
+        internal override int DefaultHP
+        {
+            get { return 15; }
+        }
+
+        internal override int DefaultMP
+        {
+            get { return 0; }
+        }
+
+        public override void OnTick(EventArgs args)
+        {
+            if (!Menu.Item("use" + Name).GetValue<bool>() || !IsReady())
+                return;
+
+            foreach (var hero in Activator.Allies())
+            {
+                if (hero.Player.NetworkId == Player.NetworkId)
+                {
+                    if (hero.Attacker == null)
+                        continue;
+
+                    if (!Parent.Item(Parent.Name + "useon" + hero.Player.ChampionName).GetValue<bool>())
+                        continue;
+
+                    if (hero.Attacker.Distance(hero.Player.ServerPosition) > Range)
+                        continue;
+
+                    if (Menu.Item("use" + Name + "norm").GetValue<bool>())
+                    {
+                        if (hero.IncomeDamage > 0 && hero.HitTypes.Contains(HitType.Danger))
+                            CastOnBestTarget((Obj_AI_Hero)hero.Attacker);
+                    }
+
+                    if (Menu.Item("use" + Name + "ulti").GetValue<bool>())
+                    {
+                        if (hero.IncomeDamage > 0 && hero.HitTypes.Contains(HitType.Ultimate))
+                            CastOnBestTarget((Obj_AI_Hero)hero.Attacker);
+                    }
+
+                    if (hero.Player.Health/hero.Player.MaxHealth*100 <=
+                        Menu.Item("selflowhp" + Name + "pct").GetValue<Slider>().Value)
+                    {
+                        if (hero.IncomeDamage > 0 && hero.HitTypes.Contains(HitType.Ultimate))
+                                CastOnBestTarget((Obj_AI_Hero)hero.Attacker);
+                    }
+
+                    if (hero.IncomeDamage > hero.Player.Health)
+                    {
+                        CastOnBestTarget((Obj_AI_Hero)hero.Attacker); 
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 【红叶推介】SP预判切换/SPredictioner.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace SPredictioner
+{
+    public class SPredictioner
+    {
+        public static Spell[] Spells = { null, null, null, null };
+        public static Menu Config;
+
+        public static void Initialize()
+        {
+            #region Initialize Menu
+            Config = new Menu("【红叶推介】SP预判", "spredictioner", true);
+            TargetSelector.AddToMenu(Config.SubMenu("Target Selector"));
+            Config.AddItem(new MenuItem("COMBOKEY", "连招键位").SetValue(new KeyBind(32, KeyBindType.Press)));
+            Config.AddItem(new MenuItem("HARASSKEY", "骚扰键位").SetValue(new KeyBind('C', KeyBindType.Press)));
+            Config.AddItem(new MenuItem("ENABLED", "启用").SetValue(true));
+            SPrediction.Prediction.Initialize(Config);
+            Config.SubMenu("SPRED").AddItem(new MenuItem("SPREDHITC", "命中率").SetValue(new StringList(ShineCommon.Utility.HitchanceNameArray, 2)));
+            Config.AddToMainMenu();
+            #endregion
+
+            #region Initialize Events
+            Spellbook.OnCastSpell += EventHandlers.Spellbook_OnCastSpell;
+            Obj_AI_Hero.OnProcessSpellCast += EventHandlers.Obj_AI_Hero_OnProcessSpellCast;
+            #endregion
+
+            #region Initialize Spells
+            foreach (var spell in SpellDatabase.Spells)
+            {
+                if (spell.ChampionName == ObjectManager.Player.CharData.BaseSkinName)
+                {
+                    Spells[(int)spell.Slot] = new Spell(spell.Slot, spell.Range);
+                    Spells[(int)spell.Slot].SetSkillshot(spell.Delay / 1000, spell.Radius, spell.MissileSpeed, spell.Collisionable, spell.Type);
+                }
+            }
+            #endregion
+        }
+    }
+}
