1c4281021b2a418145135625028f963b77f25f23
ergou <654955321@qq.com>
2015-10-05 15:23:40 +0800

    new

---
+++ 【红叶推介】Korean 卢西安/Lucian.cs
@@ -0,0 +1,29 @@
+﻿namespace KoreanLucian
+{
+    using KoreanCommon;
+
+    internal class Lucian : CommonChampion
+    {
+        public readonly Core core;
+
+        private readonly Drawing drawing;
+
+        public readonly KillSteal killSteal;
+
+        public readonly SemiAutomaticE semiAutomaticE;
+
+        public Lucian()
+            : base("【红叶推介】Korean 卢西安")
+        {
+            KoreanLucian.Spells.Load(this);
+            core = new Core(this);
+            CustomMenu.Load(this);
+            ForceUltimate.ForceUltimate = core.Ultimate;
+            drawing = new Drawing(this);
+            killSteal = new KillSteal(this);
+            DrawDamage.AmountOfDamage = KoreanLucian.Spells.MaxComboDamage;
+            semiAutomaticE = new SemiAutomaticE(this);
+            ExtendedQ.Load(this);
+        }
+    }
+}
